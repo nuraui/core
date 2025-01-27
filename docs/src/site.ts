5ee8e042eed56fc0b9576e95312cdb634fcac7c0
Akayy <techakayy@gmail.com>
2025-01-27 18:39:15 +1100

    docs: :memo: Added new Recipes set of pages, with the first recipe post

--- docs/src/site.ts
+++ docs/src/site.ts
@@ -21,6 +21,7 @@ const site = {
     { text: 'Guide', link: '/guide' },
     { text: 'Config', link: '/config' },
     { text: 'FAQs', link: '/faqs' },
+    { text: 'Recipes', link: '/recipes' },
   ],
 
   sidebar: [
@@ -55,6 +56,21 @@ const site = {
         { text: 'Troubleshooting', link: '/faqs/troubleshooting' },
       ],
     },
+    {
+      text: 'Recipes',
+      link: '/recipes',
+      children: [
+        {
+          text: 'Recipes',
+          link: '/recipes'
+        },
+        {
+          text: 'Vanilla Vue to Îles',
+          link: '/recipes/vanilla-vue-to-iles'
+        }
+      ],
+    },    
+    
   ],
 }
 
